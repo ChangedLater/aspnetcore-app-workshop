7d4ba58e031c3ef1749bbd50d7844a41b9e90ee3
Adrian Blackburn <adrian.blackburn@zeiss.com>
2017-08-15 17:48:01 +0930

    Changed Timezone to seed database with current offset

--- save-points/4-Authentication-and-Tag-Helpers/ConferencePlanner/BackEnd/Data/NDCSydneyData.cs
+++ save-points/4-Authentication-and-Tag-Helpers/ConferencePlanner/BackEnd/Data/NDCSydneyData.cs
@@ -289,7 +289,7 @@ namespace BackEnd
                 var sessionGroups = new List<SessionGroup>();
 
                 // 9:00 - 10:00
-                var startTime = new DateTimeOffset(2017, 6, 14, 9, 0, 0, TimeSpan.FromHours(1));
+                var startTime = new DateTimeOffset(2017, 6, 14, 9, 0, 0, DateTimeOffset.Now.Offset);
 
                 // Wed
                 sessionGroups.Add(new SessionGroup
